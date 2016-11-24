3f893c4277b08428c5a29c904b7562678ad466b4
John Wesley Salvador <John Wesley Salvador>
2016-11-24 17:00:10 +1100

    fixed webpack require

--- server/config/webpack.js
+++ server/config/webpack.js
@@ -1,10 +1,10 @@
-const webpack = require('webpack');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleware = require('webpack-hot-middleware');
 const config = require('../../webpack.dev.config.js')
 
 module.exports = function (app) {
   if (process.env.NODE_ENV !== 'production') {
+    const webpack = require('webpack');
+    const webpackDevMiddleware = require('webpack-dev-middleware');
+    const webpackHotMiddleware = require('webpack-hot-middleware');
     const compiler = webpack(config)
     app.use(webpackHotMiddleware(compiler))
     app.use(webpackDevMiddleware(compiler, {
